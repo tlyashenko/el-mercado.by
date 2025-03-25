555bc24e5d27aaecee28459b7bb280182fa93200
Tsmafei Liashenka <tsimafei.liashenka@gmail.com>
2025-03-25 15:16:00 +0300

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -28,7 +29,7 @@ use Laravel\Sanctum\HasApiTokens;
  *
  * @package App\Models
  */
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
