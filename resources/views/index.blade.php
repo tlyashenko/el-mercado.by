d0206a343d80546eebe35b17bb587c170b9309bf
Tsmafei Liashenka <tsimafei.liashenka@gmail.com>
2025-03-31 10:56:13 +0300

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,30 @@
 @section('content')
     {!! $page->content !!}
 
+    <section class="section__about">
+        <div class="container">
+            <div class="about__name">
+                <h2>Как купить хамон в Минске?</h2>
+            </div>
+            <div class="about__intro">
+                <p>Просто выберите сорт хамона, оформите заказ на сайте, и наш курьер доставит его вам в удобное время. Мы предлагаем разнообразие популярных сортов хамона: от классического Серрано до премиального Иберико. Убедитесь в качестве наших продуктов сами!</p>
+
+                <p><strong>Почему наши клиенты выбирают нас?</strong></p>
+
+                <ul>
+                    <li>Лучшие цены на хамон в Минске.</li>
+                    <li>Экспресс-доставка хамона в Минск с гарантией свежести.</li>
+                    Надежность и отличное обслуживание.</li>
+                </ul>
+
+                <p>Не упустите шанс побаловать себя или своих близких лучшими деликатесами из Европы! Сделайте заказ прямо сейчас, чтобы купить хамон в Минске с доставкой по лучшей цене.</p>
+            </div>
+            <picture class="about__img">
+                <source media="(min-width: 768px)" srcset="static/images/content/about--desk.webp"><img src="static/images/content/about--mobile.webp" alt="Mercado" loading="lazy">
+            </picture>
+        </div>
+    </section>
+
     <section class="section__why">
         <div class="container">
             <div class="why__wrap">
